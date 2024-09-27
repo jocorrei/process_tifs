940ef41cd0811b051e4165fdc992706052df4c6c
jocorrei <mgjoao41@gmail.com>
2024-09-27 11:15:08 +0100

    more nasty things

--- process_images.py
+++ process_images.py
@@ -7,7 +7,6 @@ def convert_tif_to_jpg(directory):
         print(dirs)
         for dir_name in dirs:
             if dir_name.lower() == 'jpg':
-                print("Did I got here?", dir_name)
                 tipo_jpg_path = os.path.join(root, dir_name)
                 convert_images_in_tipo_jpg(tipo_jpg_path)
 
@@ -15,7 +14,6 @@ def convert_tif_to_jpg(directory):
 def convert_images_in_tipo_jpg(tipo_jpg_path):
     for root, dirs, files in os.walk(tipo_jpg_path):
         for file in files:
-            print("\n", file)
             if file.lower().endswith('.tif') or file.lower().endswith('.tiff'):
                 file_path = os.path.join(root, file)
                 new_file_path = os.path.splitext(file_path)[0] + '.jpg'
@@ -24,6 +22,9 @@ def convert_images_in_tipo_jpg(tipo_jpg_path):
                     img.convert('RGB').save(new_file_path, 'JPEG')
 
                 print(f"Converted {file_path} to {new_file_path}")
+                # Remove the original .tif file
+                os.remove(file_path)
+                print(f"Removed {file_path}")
 
 
 def main():
