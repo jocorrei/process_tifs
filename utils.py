d9ecbd42c9e06a120f344f0c75df923f5e795dc3
jocorrei <mgjoao41@gmail.com>
2024-10-05 13:23:57 +0100

    removed unused function

--- utils.py
+++ utils.py
@@ -8,9 +8,3 @@ def convert_jpeg_to_tiff(jpeg_path, output_dir):
         img.save(tiff_path, format='TIFF')
     return tiff_path
 
-def convert_tiff_to_jpeg(tiff_path, output_dir):
-    jpeg_path = os.path.join(output_dir, os.path.basename(tiff_path).rsplit('.', 1)[0] + '.jpg')
-    with Image.open(tiff_path) as img:
-        img = img.convert('RGB')
-        img.save(jpeg_path, format='JPEG')
-    return jpeg_path
